62d3642334ee1a4a6895b87a44d866f6b148aab7
Geraldine Craig <gerriemcraig@outlook.com>
2020-09-03 22:57:42 -0700

    Added JavaScript from CodeSandbox

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+import Card from "react-bootstrap/Card";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <small>
+        <a href="https://github.com/KissaSpirit/weather-app" target="_blank">
+          Open-source code
+        </a>{" "}
+        by SheCodes & Geraldine Craig
+      </small>
+    </div>
+  );
+}
