afbd08d9a3708f835aae9bfeb5a887903bac0680
Geraldine Craig <gerriemcraig@outlook.com>
2020-09-03 23:07:13 -0700

    Installed react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
+
+import Header from "./Header";
 import Weather from "./Weather";
+import Degrees from "./Degrees";
+import Stats from "./Stats";
+import Footer from "./Footer";
 
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
+import Card from "react-bootstrap/Card";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello world
-        </h1>
-        <Weather />
-      </header>
+      <h1 class="display-4">
+        Geraldine's <small className="text-muted">SheCodes Weather App</small>
+      </h1>
+      <hr />
+
+      <Card>
+        <Card.Header className="card-header">
+          <Header />
+        </Card.Header>
+        <Card.Body className="card-body">
+          <Card.Text>
+            <Weather />
+          </Card.Text>
+          <Card.Title>
+            <Degrees />
+          </Card.Title>
+          <Card.Text>
+            <Stats />
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer className="text-muted">
+          <Footer />
+        </Card.Footer>
+      </Card>
     </div>
   );
 }
-
-export default App;
